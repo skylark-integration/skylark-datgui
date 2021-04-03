51e97921236aa5dd7051f332c1ad19f6b44c9bbe
hudaokeji <github@hudaokeji.com>
2021-04-03 20:19:53 +0900

    update

--- dist/uncompressed/skylark-datgui-all.js
+++ dist/uncompressed/skylark-datgui-all.js
@@ -1309,12 +1309,50 @@ define('skylark-langx/arrays',[
 ],function(arrays){
   return arrays;
 });
-define('skylark-langx-klass/klass',[
-  "skylark-langx-ns/ns",
+define('skylark-langx-constructs/constructs',[
+  "skylark-langx-ns"
+],function(skylark){
+
+    return skylark.attach("langx.constructs",{});
+});
+define('skylark-langx-constructs/inherit',[
+	"./constructs"
+],function(constructs){
+
+    function inherit(ctor,base) {
+        ///var f = function() {};
+        ///f.prototype = base.prototype;
+        ///
+        ///ctor.prototype = new f();
+
+	    if ((typeof base !== "function") && base) {
+	      throw new TypeError("Super expression must either be null or a function");
+	    }
+
+	    ctor.prototype = Object.create(base && base.prototype, {
+	      constructor: {
+	        value: ctor,
+	        writable: true,
+	        configurable: true
+	      }
+	    });
+
+	    if (base) {
+	    	//tor.__proto__ = base;
+	    	Object.setPrototypeOf(ctor, base);
+	    } 
+    }
+
+    return constructs.inherit = inherit
+});
+define('skylark-langx-constructs/klass',[
+  "skylark-langx-ns",
   "skylark-langx-types",
   "skylark-langx-objects",
   "skylark-langx-arrays",
-],function(skylark,types,objects,arrays){
+  "./constructs",
+  "./inherit"
+],function(skylark,types,objects,arrays,constructs,inherit){
     var uniq = arrays.uniq,
         has = objects.has,
         mixin = objects.mixin,
@@ -1367,12 +1405,7 @@ let longEar = klass({
 },rabbit);
 */
     
-    function inherit(ctor, base) {
-        var f = function() {};
-        f.prototype = base.prototype;
 
-        ctor.prototype = new f();
-    }
 
     var f1 = function() {
         function extendClass(ctor, props, options) {
@@ -1465,8 +1498,10 @@ let longEar = klass({
             var newCtor =ctor;
             for (var i=0;i<mixins.length;i++) {
                 var xtor = new Function();
-                xtor.prototype = Object.create(newCtor.prototype);
-                xtor.__proto__ = newCtor;
+
+                inherit(xtor,newCtor)
+                //xtor.prototype = Object.create(newCtor.prototype);
+                //xtor.__proto__ = newCtor;
                 xtor.superclass = null;
                 mixin(xtor.prototype,mixins[i].prototype);
                 xtor.prototype.__mixin__ = mixins[i];
@@ -1521,15 +1556,17 @@ let longEar = klass({
 
 
             // Populate our constructed prototype object
-            ctor.prototype = Object.create(innerParent.prototype);
+            ///ctor.prototype = Object.create(innerParent.prototype);
 
             // Enforce the constructor to be what we expect
-            ctor.prototype.constructor = ctor;
-            ctor.superclass = parent;
-
+            ///ctor.prototype.constructor = ctor;
+  
             // And make this class extendable
-            ctor.__proto__ = innerParent;
+            ///ctor.__proto__ = innerParent;
 
+            inherit(ctor,innerParent);
+
+            ctor.superclass = parent;
 
             if (!ctor._constructor) {
                 ctor._constructor = _constructor;
@@ -1558,7 +1595,15 @@ let longEar = klass({
 
     var createClass = f1();
 
-    return skylark.attach("langx.klass",createClass);
+    return constructs.klass = createClass;
+});
+define('skylark-langx-klass/klass',[
+  "skylark-langx-ns",
+  "skylark-langx-constructs/klass"
+],function(skylark,klass){
+
+
+    return skylark.attach("langx.klass",klass);
 });
 define('skylark-langx-klass/main',[
 	"./klass"
@@ -2899,294 +2944,6 @@ define('skylark-langx/binary',[
 	"skylark-langx-binary"
 ],function(binary){
   return binary;
-});
-define('skylark-langx-constructs/constructs',[
-  "skylark-langx-ns"
-],function(skylark){
-
-    return skylark.attach("langx.constructs",{});
-});
-define('skylark-langx-constructs/inherit',[
-	"./constructs"
-],function(constructs){
-
-    function inherit(ctor,base) {
-        ///var f = function() {};
-        ///f.prototype = base.prototype;
-        ///
-        ///ctor.prototype = new f();
-
-	    if ((typeof base !== "function") && base) {
-	      throw new TypeError("Super expression must either be null or a function");
-	    }
-
-	    ctor.prototype = Object.create(base && base.prototype, {
-	      constructor: {
-	        value: ctor,
-	        writable: true,
-	        configurable: true
-	      }
-	    });
-
-	    if (base) {
-	    	//tor.__proto__ = base;
-	    	Object.setPrototypeOf(ctor, base);
-	    } 
-    }
-
-    return constructs.inherit = inherit
-});
-define('skylark-langx-constructs/klass',[
-  "skylark-langx-ns",
-  "skylark-langx-types",
-  "skylark-langx-objects",
-  "skylark-langx-arrays",
-  "./constructs",
-  "./inherit"
-],function(skylark,types,objects,arrays,constructs,inherit){
-    var uniq = arrays.uniq,
-        has = objects.has,
-        mixin = objects.mixin,
-        isArray = types.isArray,
-        isDefined = types.isDefined;
-
-/* for reference 
- function klass(props,parent) {
-    var ctor = function(){
-        this._construct();
-    };
-    ctor.prototype = props;
-    if (parent) {
-        ctor._proto_ = parent;
-        props.__proto__ = parent.prototype;
-    }
-    return ctor;
-}
-
-// Type some JavaScript code here.
-let animal = klass({
-  _construct(){
-      this.name = this.name + ",hi";
-  },
-    
-  name: "Animal",
-  eat() {         // [[HomeObject]] == animal
-    alert(`${this.name} eats.`);
-  }
-    
-    
-});
-
-
-let rabbit = klass({
-  name: "Rabbit",
-  _construct(){
-      super._construct();
-  },
-  eat() {         // [[HomeObject]] == rabbit
-    super.eat();
-  }
-},animal);
-
-let longEar = klass({
-  name: "Long Ear",
-  eat() {         // [[HomeObject]] == longEar
-    super.eat();
-  }
-},rabbit);
-*/
-    
-
-
-    var f1 = function() {
-        function extendClass(ctor, props, options) {
-            // Copy the properties to the prototype of the class.
-            var proto = ctor.prototype,
-                _super = ctor.superclass.prototype,
-                noOverrided = options && options.noOverrided,
-                overrides = options && options.overrides || {};
-
-            for (var name in props) {
-                if (name === "constructor") {
-                    continue;
-                }
-
-                // Check if we're overwriting an existing function
-                var prop = props[name];
-                if (typeof props[name] == "function") {
-                    proto[name] =  !prop._constructor && !noOverrided && typeof _super[name] == "function" ?
-                          (function(name, fn, superFn) {
-                            return function() {
-                                var tmp = this.overrided;
-
-                                // Add a new ._super() method that is the same method
-                                // but on the super-class
-                                this.overrided = superFn;
-
-                                // The method only need to be bound temporarily, so we
-                                // remove it when we're done executing
-                                var ret = fn.apply(this, arguments);
-
-                                this.overrided = tmp;
-
-                                return ret;
-                            };
-                        })(name, prop, _super[name]) :
-                        prop;
-                } else if (types.isPlainObject(prop) && prop!==null && (prop.get)) {
-                    Object.defineProperty(proto,name,prop);
-                } else {
-                    proto[name] = prop;
-                }
-            }
-            return ctor;
-        }
-
-        function serialMixins(ctor,mixins) {
-            var result = [];
-
-            mixins.forEach(function(mixin){
-                if (has(mixin,"__mixins__")) {
-                     throw new Error("nested mixins");
-                }
-                var clss = [];
-                while (mixin) {
-                    clss.unshift(mixin);
-                    mixin = mixin.superclass;
-                }
-                result = result.concat(clss);
-            });
-
-            result = uniq(result);
-
-            result = result.filter(function(mixin){
-                var cls = ctor;
-                while (cls) {
-                    if (mixin === cls) {
-                        return false;
-                    }
-                    if (has(cls,"__mixins__")) {
-                        var clsMixines = cls["__mixins__"];
-                        for (var i=0; i<clsMixines.length;i++) {
-                            if (clsMixines[i]===mixin) {
-                                return false;
-                            }
-                        }
-                    }
-                    cls = cls.superclass;
-                }
-                return true;
-            });
-
-            if (result.length>0) {
-                return result;
-            } else {
-                return false;
-            }
-        }
-
-        function mergeMixins(ctor,mixins) {
-            var newCtor =ctor;
-            for (var i=0;i<mixins.length;i++) {
-                var xtor = new Function();
-
-                inherit(xtor,newCtor)
-                //xtor.prototype = Object.create(newCtor.prototype);
-                //xtor.__proto__ = newCtor;
-                xtor.superclass = null;
-                mixin(xtor.prototype,mixins[i].prototype);
-                xtor.prototype.__mixin__ = mixins[i];
-                newCtor = xtor;
-            }
-
-            return newCtor;
-        }
-
-        function _constructor ()  {
-            if (this._construct) {
-                return this._construct.apply(this, arguments);
-            } else  if (this.init) {
-                return this.init.apply(this, arguments);
-            }
-        }
-
-        return function createClass(props, parent, mixins,options) {
-            if (isArray(parent)) {
-                options = mixins;
-                mixins = parent;
-                parent = null;
-            }
-            parent = parent || Object;
-
-            if (isDefined(mixins) && !isArray(mixins)) {
-                options = mixins;
-                mixins = false;
-            }
-
-            var innerParent = parent;
-
-            if (mixins) {
-                mixins = serialMixins(innerParent,mixins);
-            }
-
-            if (mixins) {
-                innerParent = mergeMixins(innerParent,mixins);
-            }
-
-            var klassName = props.klassName || "",
-                ctor = new Function(
-                    "return function " + klassName + "() {" +
-                    "var inst = this," +
-                    " ctor = arguments.callee;" +
-                    "if (!(inst instanceof ctor)) {" +
-                    "inst = Object.create(ctor.prototype);" +
-                    "}" +
-                    "return ctor._constructor.apply(inst, arguments) || inst;" + 
-                    "}"
-                )();
-
-
-            // Populate our constructed prototype object
-            ///ctor.prototype = Object.create(innerParent.prototype);
-
-            // Enforce the constructor to be what we expect
-            ///ctor.prototype.constructor = ctor;
-  
-            // And make this class extendable
-            ///ctor.__proto__ = innerParent;
-
-            inherit(ctor,innerParent);
-
-            ctor.superclass = parent;
-
-            if (!ctor._constructor) {
-                ctor._constructor = _constructor;
-            } 
-
-            if (mixins) {
-                ctor.__mixins__ = mixins;
-            }
-
-            if (!ctor.partial) {
-                ctor.partial = function(props, options) {
-                    return extendClass(this, props, options);
-                };
-            }
-            if (!ctor.inherit) {
-                ctor.inherit = function(props, mixins,options) {
-                    return createClass(props, this, mixins,options);
-                };
-            }
-
-            ctor.partial(props, options);
-
-            return ctor;
-        };
-    }
-
-    var createClass = f1();
-
-    return constructs.klass = createClass;
 });
 define('skylark-langx-constructs/main',[
 	"./constructs",
